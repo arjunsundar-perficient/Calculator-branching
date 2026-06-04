fb5cfd988c9e7b56a042554d1810b9fa05c47e67
Arjun Sundar <arjun.sundar@perficient.com>
2026-06-04 10:40:17 +0530

    Added subtraction

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ num2 = int(input(print("Enter Second Number")))
 op = input(print("Choose from (+)"))
 if op == "+":
     result = num1 + num2
+elif op == "-":
+    result = num1 + num2
 else:
     result = "Invalid operation"
 print(result)
