d431e8b9dff0d4be12687dfd2b944394bf48609c
Arjun Sundar <arjun.sundar@perficient.com>
2026-06-04 10:27:44 +0530

    Addition calculator

---
+++ main.py
@@ -0,0 +1,8 @@
+num1 = int(input(print("Enter first number")))
+num2 = int(input(print("Enter Second Number")))
+op = input(print("Choose from (+)"))
+if op == "+":
+    result = num1 + num2
+else:
+    result = "Invalid operation"
+print(result)
